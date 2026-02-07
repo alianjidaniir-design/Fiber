20469bb3cf232ed0b1ded95ac49f8f1bc6d24f29
Mahdi Anjidani <>
2026-02-07 09:53:09 +0330

    branch

--- API/2/api.go
+++ API/2/api.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"time"
 
+	"github.com/gofiber/fiber/v3"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -34,13 +35,14 @@ type Courses struct {
 type Enrollments struct {
 	gorm.Model
 	Status     EnrollmentStatus `gorm:"size : 10;default:'enrolled';not null"`
-	CanceledAt *time.Time       `gorm:"not null"`
+	CanceledAt *time.Time       `gorm:"null"`
 	EnrolledAt *time.Time       `gorm:"not null"`
-	StudentId  int              `gorm:"not null"`
-	CourseId   int              `gorm:"not null"`
+	StudentId  int
+	CourseId   int
 }
 
 func main() {
+	app := fiber.New()
 	dsn := "root:123456@tcp(127.0.0.1:3306)/ali-db?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
@@ -54,7 +56,11 @@ func main() {
 		panic("failed to connect database")
 	}
 
-	err = gorm.G[Students](db).Create(ctx, &Students{StudentCode: "404161429", FirstName: "Mahdi", LastName: "Miladi"})
-	err = gorm.G[Courses](db).Create(ctx, &Courses{CourseCode: "169", Title: "Arabic", Capacity: 123, EnrolledCount: 1, IsActive: true})
+	err = gorm.G[Students](db).Create(ctx, &Students{StudentCode: "404161494", FirstName: "Mahdi", LastName: "Miladi"})
+	err = gorm.G[Courses](db).Create(ctx, &Courses{CourseCode: "11", Title: "Arabic", Capacity: 123, EnrolledCount: 1, IsActive: true})
+	err = gorm.G[Enrollments](db).Create(ctx, &Enrollments{Status: StatusEnrolled, CanceledAt: nil, StudentId: 1, CourseId: 1})
 
+	app.Post("/api/v1/students", func(c *fiber.Ctx) error {
+
+	})
 }
