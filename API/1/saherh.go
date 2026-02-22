8f4d33aec01c461aaf220e56f5f9d6d442244629
Mahdi Anjidani <>
2026-02-22 12:42:35 +0330

    branch

--- API/1/saherh.go
+++ API/1/saherh.go
@@ -1,1 +1,32 @@
-package _
+package main
+
+import (
+	"strings"
+
+	"github.com/gofiber/fiber/v3"
+)
+
+func compress(a any) any {
+	switch a.(type) {
+	case int:
+		return a.(int)*a.(int) + 1
+	case float64:
+		return a
+	case string:
+		return strings.ToLower(a.(string))
+	default:
+		return a
+	}
+
+}
+
+func fib(c fiber.Ctx) error {
+	f1 := c.Query("f1")
+	g := compress(f1)
+	return c.JSON(fiber.Map{"data": g})
+}
+func main() {
+	app := fiber.New()
+	app.Get("/compress", fib)
+	app.Listen(":3001")
+}
