2c7e3062669bb0065a1dacb5e60116ed5f97ab2e
Mahdi Anjidani <>
2026-03-12 14:00:30 +0330

    branch

---
+++ API/2/models/student/datasourse/memoryDS/studentDBDS.go
@@ -0,0 +1,40 @@
+package memoryDS
+
+import (
+	"Fiber/API/2/apiSchema/studentsSchema"
+	studentDataModel "Fiber/API/2/models/student/dataModel"
+	"context"
+	"sync"
+	"sync/atomic"
+	"time"
+)
+
+type StudentDBDS struct {
+	idCounter int64
+	students  []studentDataModel.Studentss
+	lock      sync.RWMutex
+}
+
+func NewStudentDBDS(startID int64) *StudentDBDS {
+	return &StudentDBDS{
+		idCounter: startID,
+		students:  []studentDataModel.Studentss{},
+	}
+}
+
+func (db *StudentDBDS) CreateStudent(ctx context.Context, req studentsSchema.CreateUserRequest) (studentDataModel.Studentss, error) {
+	_ = ctx
+
+	student := studentDataModel.Studentss{
+		ID:        atomic.AddInt64(&db.idCounter, 1),
+		FirstName: req.FirstName,
+		LastName:  req.LastName,
+		CreatedAt: time.Now().Format("2006-01-02 15:04:05"),
+	}
+
+	db.lock.Lock()
+	db.students = append(db.students, student)
+	db.lock.Unlock()
+	return student, nil
+
+}
--- API/2/models/student/datasourse/userDS.go
+++ API/2/models/student/datasourse/userDS.go
@@ -4,9 +4,9 @@ import (
 	"Fiber/API/2/apiSchema/studentsSchema"
 	"context"
 
-	studentDataModel "github.com/alianjidaniir-design/Fiber/API/2/models/student/dataModel"
+	studentDataModel "Fiber/API/2/models/student/dataModel"
 )
 
-type StudentDBDS interface{
-	CreateStudent(ctx context.Context, req studentsSchema.CreateUserRequest ) (studentDataModel., error)
+type StudentDBDS interface {
+	CreateStudent(ctx context.Context, req studentsSchema.CreateUserRequest) (studentDataModel.Studentss, error)
 }
